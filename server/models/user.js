b3b644c410f259a1b34c38d8627a336dc3a97c17
Vincenzo Gambino <gambinovin@gmail.com>
2018-11-28 16:52:48 +0100

    User JWT

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,75 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator =  require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+
+/**
+ * User schema.
+ * - email
+ * - password
+ * - tokens
+ * -- access
+ * -- token
+ */
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    required:true,
+    required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} not a valid email.'
+    },
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens:[{
+    access: {
+      type: String,
+      require: true
+    },
+    token: {
+      type: String,
+      require: true
+    }
+  }]
 });
+
+/**
+ * Override .toJSON method to return only id and email.
+ *
+ * @returns Object
+ */
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+/**
+ * Generates auth token upon user save.
+ *
+ * @returns Promise.
+ */
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({access, token});
+
+    return user.save().then(() => {
+      return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
